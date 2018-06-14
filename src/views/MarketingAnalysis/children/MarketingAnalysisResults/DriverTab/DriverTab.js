02e02a282a4c85a485491dd51e07f4675e9a49f7
Jonathan <jleack@gmail.com>
2018-06-14 09:52:40 -0700

    Created dynamic tab structure

---
+++ src/views/MarketingAnalysis/children/MarketingAnalysisResults/DriverTab/DriverTab.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+    NavLink,
+    NavItem,
+ } from 'reactstrap';
+import classnames from 'classnames';
+
+/*======================================================================
+// This will display the tab for each input driver along with the name
+// and a handler for toggling between the display of data underneath
+// each tab.
+======================================================================*/
+class DriverTab extends Component {
+
+    render() {
+        return (
+            <NavItem>
+                <NavLink className={classnames({ active: this.props.activeDriver === `${ this.props.tabNum }` })} onClick={() => { this.props.toggleDriverTab(`${ this.props.tabNum }`); }}>
+                    { this.props.tabName }
+                </NavLink>
+            </NavItem>
+        );
+    }
+}
+
+export default DriverTab;
